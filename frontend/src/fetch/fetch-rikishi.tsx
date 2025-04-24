22cce3fcffd8e2900a9d5fe3523c04ea50a45f7c
hyacinth-07 <ndr.hya37@gmail.com>
2025-04-24 11:28:58 +0200

    Add fetchRikishi function to retrieve data from Sumo API

---
+++ frontend/src/fetch/fetch-rikishi.tsx
@@ -0,0 +1,13 @@
+export const fetchRikishi = async () => {
+
+    const response = await fetch("https://sumo-api.com/api/rikishi/17");
+
+    if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.statusText} | ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data
+
+}
