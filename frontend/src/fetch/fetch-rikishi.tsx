72763fb50333b9b98ba1c0478e0bc98a425702e1
hyacinth-07 <ndr.hya37@gmail.com>
2025-04-24 11:56:44 +0200

    Update fetchRikishi function to use correct API endpoint for fetching rikishi data

--- frontend/src/fetch/fetch-rikishi.tsx
+++ frontend/src/fetch/fetch-rikishi.tsx
@@ -1,6 +1,7 @@
 export const fetchRikishi = async () => {
 
-    const response = await fetch("https://sumo-api.com/api/rikishi/17");
+    // const response = await fetch("https://sumo-api.com/api/rikishi/17");
+    const response = await fetch("https://sumo-api.com/api/rikishis?shikonaEn=Endo");
 
     if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText} | ${response.status}`);
