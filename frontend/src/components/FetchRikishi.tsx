e3664c3d73f58d4f3854fad669eea515f375ddc3
hyacinth-07 <ndr.hya37@gmail.com>
2025-04-24 11:50:56 +0200

    Implement handleClick function in FetchRikishi component to fetch and display data

--- frontend/src/components/FetchRikishi.tsx
+++ frontend/src/components/FetchRikishi.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import { fetchRikishi } from "../fetch/fetch-rikishi";
+
 export default function FetchRikishi() {
+
+    const [text, setText] = useState("");
+    
+    const handleClick = async () => {
+
+        try {
+            fetchRikishi().then((data) => setText(data))
+            console.log(text)
+        } catch (error) {
+            throw new Error(`${error}`)
+        }
+    }
+        
 
     return (
     <div>
-        <button>Find one!</button>
+        <button onClick={handleClick}>Find one!</button>
+        {text && (
+            <pre> 
+                {JSON.stringify(text, null, 2)}
+            </pre>
+            )}
     </div>
     )
 }
