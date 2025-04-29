b3c148e40455f573acd7a18a6369511a4183017f
hyacinth-07 <ndr.hya37@gmail.com>
2025-04-29 14:28:04 +0200

    Remove commented-out code for fetching rikishi data in fetchAllRikishi function

--- frontend/src/fetch/fetch-all-rikishi.tsx
+++ frontend/src/fetch/fetch-all-rikishi.tsx
@@ -25,16 +25,4 @@ export const fetchAllRikishi = async (intai: boolean = false) => {
 
         return data;
     }
-
-
-    // const response = await fetch("https://sumo-api.com/api/rikishis");
-
-    //     if (!response.ok) {
-    //         throw new Error(`Error fetching data: ${response.statusText} | ${response.status}`);
-    //     }
-
-    //     const data = await response.json();
-
-    //     return data;
-
 }
