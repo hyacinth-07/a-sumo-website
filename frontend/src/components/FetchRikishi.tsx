ab0c7bf34ce121e545355b00ee2bf51e7f59d05b
hyacinth-07 <ndr.hya37@gmail.com>
2025-04-30 11:02:21 +0200

    Refactor FetchRikishi component to improve autocomplete functionality and remove unused useEffect; add populate-suggestions utility file

--- frontend/src/components/FetchRikishi.tsx
+++ frontend/src/components/FetchRikishi.tsx
@@ -7,20 +7,32 @@ export default function FetchRikishi() {
     const [result, setResult] = useState("");
     const [searchedName, setSearchedName] = useState("")
     const [suggestions, setSuggestions] = useState(Array)
+    const [autocomplete, setAutocomplete] = useState(Array)
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     try {
+    //         fetchAllRikishi().then((data) => setSuggestions(data.records))
+    //     } catch (error) {
+    //         throw new Error(`${error}`)
+    //     }
+    // }, [])
+
+    const searchOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
         try {
-            fetchAllRikishi(true).then((data) => setSuggestions(data.records))
+            fetchAllRikishi().then((data) => setSuggestions(data.records))
         } catch (error) {
             throw new Error(`${error}`)
         }
-    }, [])
 
-    const searchOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         if (typeof value === "string" || value === "") {
             setSearchedName(value);
         } 
+
+
+        setAutocomplete(suggestions.filter((item) => item.shikonaEn?.toLowerCase().includes(searchedName.toLowerCase())).map(item => <p key={item.id}>{item.shikonaEn}, {item.currentRank}</p>))
+
     };
     
     const handleClick = async (name: string) => {
@@ -32,6 +44,9 @@ export default function FetchRikishi() {
         }
 
     }
+
+
+    console.log(autocomplete)
         
     return (
     <div>
@@ -39,9 +54,9 @@ export default function FetchRikishi() {
             <input type="text" name="rikishiName" id="rikishiName" value={searchedName} onChange={searchOnChange}/>
         </label>
         <button onClick={() => handleClick(searchedName)}>Find one!</button>
-        <div>
+        {/* <div>
             {suggestions.filter((item) => item.shikonaEn?.toLowerCase().includes(searchedName.toLowerCase())).map(item => <p key={item.id}>{item.shikonaEn}, {item.currentRank}</p>)}
-        </div>
+        </div> */}  
         {result && (
             <pre> 
                 {JSON.stringify(result, null, 2)}
